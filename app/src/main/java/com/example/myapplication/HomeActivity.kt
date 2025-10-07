d4d96f80ee4d76d57613acded8dc57097b8606ce
Fabricio <fabriciogarciacarvalho87@hotmail.com>
2025-10-07 11:37:26 -0300

    criação da activity e tela da home

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -0,0 +1,45 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class HomeActivity : AppCompatActivity() {
+
+    lateinit var consultarAcervoRecyclerView: RecyclerView
+    lateinit var reservasRecyclerView: RecyclerView
+    lateinit var submeterProducaoRecyclerView: RecyclerView
+    lateinit var meusEmprestimosRecyclerView: RecyclerView
+    lateinit var exposicaoDosAlunosRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+    private fun setupRecyclerViews(){
+        //Consultar Acervo
+        consultarAcervoRecyclerView = findViewById(R.id.btn_search_acervos)
+        consultarAcervoRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
+
+        //reservas
+        reservasRecyclerView = findViewById(R.id.btn_reservations)
+        reservasRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
+
+        //submeter producao
+        submeterProducaoRecyclerView = findViewById(R.id.btn_submit_prod)
+        submeterProducaoRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
+
+        //meus emprestimos
+        meusEmprestimosRecyclerView = findViewById(R.id.btn_my_loans)
+        meusEmprestimosRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
+
+        //exposicao dos alunos
+        exposicaoDosAlunosRecyclerView = findViewById(R.id.btn_student_productions)
+        exposicaoDosAlunosRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
+    }
+}
